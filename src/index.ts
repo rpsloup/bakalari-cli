2e441703de0e84ca18620cc34a359b99ac2948d6
rpsloup <robin.patrik.sloup@proton.me>
2022-11-08 08:21:18 +0100

    Fetching the access token from the API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+import fetch from 'node-fetch';
+
+const bakalariUrl: string = '';
+const userName: string = '';
+const userPassword: string = '';
+
+const getAccessToken = async (): Promise<string> => {
+  const res = await fetch(`${bakalariUrl}/api/login`, {
+    method: 'post',
+    headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+    body: `grant_type=password&username=${userName}&password=${userPassword}&client_id=ANDR`,
+  });
+  const data = await res.json();
+  return data?.access_token ?? '';
+}
+
+getAccessToken().then(token => console.log(token));
