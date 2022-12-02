2ffce6280eb3eff401daefcfa056585796b1a1dd
rpsloup <robin.patrik.sloup@proton.me>
2022-12-02 10:41:55 +0100

    Encrpyting saved user password

--- src/functions/dataFunctions.ts
+++ src/functions/dataFunctions.ts
@@ -1,7 +1,10 @@
 import * as fs from 'fs';
+import CryptoJS, { AES } from 'crypto-js';
+
 import type { UserAuth } from '../typings/authTypes'
 
 const DATA_FOLDER = 'data';
+const aesKey = '770A8A65DA156D24EE2A093277530142';
 
 const createDataFolder = () => {
   if (!fs.existsSync(DATA_FOLDER)){
@@ -13,12 +16,18 @@ export const loadLoginInfo = (): UserAuth | null => {
   if (!fs.existsSync(`${DATA_FOLDER}/auth.json`)) return null;
   const rawLoginData = fs.readFileSync(`${DATA_FOLDER}/auth.json`);
   const loginData = JSON.parse(rawLoginData.toString());
-  return loginData;
+  return {
+    ...loginData,
+    userPassword: loginData.userPassword.length !== 0 ? AES.decrypt(loginData.userPassword, aesKey).toString(CryptoJS.enc.Utf8) : loginData.userPassword,
+  };
 }
 
 export const saveLoginInfo = (loginInfo: UserAuth) => {
   createDataFolder();
-  fs.writeFileSync(`${DATA_FOLDER}/auth.json`, `${JSON.stringify(loginInfo, null, 2)}\n`);
+  fs.writeFileSync(`${DATA_FOLDER}/auth.json`, `${JSON.stringify({
+    ...loginInfo,
+    userPassword: loginInfo.userPassword.length !== 0 ? AES.encrypt(loginInfo.userPassword, aesKey).toString() : loginInfo.userPassword,
+  }, null, 2)}\n`);
 }
 
 export const deleteLoginInfo = () => {
